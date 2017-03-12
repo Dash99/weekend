bee83d818a4c247622b5a8acff1c11422d608b08
Kamogelo Molotsi <kmolotsi.developer@gmail.com>
2017-03-13 01:10:24 +0200

    Add files via upload

---
+++ public_html/js/js.js
@@ -0,0 +1,30 @@
+
+
+function init() {
+    refresh();
+}
+
+function refresh() {
+    
+    var showData = $('#show-data');
+    $.getJSON('data.json', function (data) {
+        console.log(data);
+        var items = data.items.map(function (item) {
+            return item.key + ': ' + item.value;
+        });
+
+        showData.empty();
+
+        if (items.length) {
+            var content = '<li>' + items.join('</li><li>') + '</li>';
+            var list = $('<ul />').html(content);
+            showData.append(list);
+        }
+    });
+    
+    showData.text('Loading the JSON file.'); 
+    
+    setTimeout("refresh()",5000);
+}
+
+   
